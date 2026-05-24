78a06cab5f22de5f5d16e9b64228f0bf3846595f
tabib-e-alahi <etabib.alahi@gmail.com>
2026-05-24 23:00:00 +0600

    fix streak

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,11 @@
-import { Badge } from "@/components/ui/badge"
-import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardAction,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
+import ProductCard from "./_components/Card";
+
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-
-
-
-        <Card className="relative mx-auto w-full max-w-sm pt-0">
-          <div className="absolute inset-0 z-30 aspect-video bg-black/35" />
-          <img
-            src="https://avatar.vercel.sh/shadcn1"
-            alt="Event cover"
-            className="relative z-20 aspect-video w-full object-cover brightness-60 grayscale dark:brightness-40"
-          />
-          <CardHeader>
-            <CardAction>
-              <Badge variant="secondary">Featured</Badge>
-            </CardAction>
-            <CardTitle>Design systems meetup</CardTitle>
-            <CardDescription>
-              A practical talk on component APIs, accessibility, and shipping
-              faster.
-            </CardDescription>
-          </CardHeader>
-          <CardFooter>
-            <Button className="w-full">View Event</Button>
-          </CardFooter>
-        </Card>
+        <ProductCard></ProductCard>
       </main>
     </div>
   );
